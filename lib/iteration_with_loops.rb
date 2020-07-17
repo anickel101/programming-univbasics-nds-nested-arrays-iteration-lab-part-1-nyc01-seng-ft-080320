adcc92521c19584fca3f159746d852a6fd4ed21d
Anson Nickel <anson.nickel@gmail.com>
2020-07-17 02:16:28 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,10 +5,9 @@ def find_even_values(src)
     while col < src[row].count do
       if src[row][col].even?
         p src[row][col]
-        col +=1
       end 
-        col += 1
+      col += 1
     end 
-      row += 1
+    row += 1
   end
 end
